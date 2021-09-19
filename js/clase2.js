f150010d7a3d3f0fb6163f2af6ff2125d4f17355
mat0015 <mat0015@hotmail.com>
2021-09-19 15:46:50 -0300

    Subiendo ejercicio clase 2

---
+++ js/clase2.js
@@ -0,0 +1,20 @@
+let numero = prompt("Ingrese un número");
+    if (numero > 1000) {
+        alert ("El número ingresado es mayor a 1000");
+    }
+else {
+}
+
+let palabra = prompt("Ingrese la contraseña");
+    if (palabra == "Hola") {
+        alert ("La contraseña es correcta");
+    }
+else {
+}
+
+let numero1 = prompt("Ingrese otro número");
+    if ((numero1 >= 10) && (numero1 <= 50)) {
+        alert ("El número ingresado está emtre 10 y 50");
+    }
+else {
+}
